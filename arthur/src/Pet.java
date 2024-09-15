2121e8f5a57e471583313247ccba61a9f9be36c5
arthur <arthurerzingerdelima@gmail.com>
2024-09-15 01:35:58 -0300

    sla

---
+++ arthur/src/Pet.java
@@ -0,0 +1,20 @@
+public class Pet {
+    private String nome;
+    private String tipo;
+
+    public Pet(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    
+}
